c464a48204e0d1f3368295290d23794819ad9bd6
Naru-hub <1208ny0324@gmail.com>
2021-06-02 05:22:38 +0000

    add book_comment

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
-  
-  resources :users,only:[:index,:show,:edit,:update] 
+
+  resources :users,only:[:index,:show,:edit,:update]
   resources :books,only:[:index,:create,:show,:edit,:update,:destroy] do
+    resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
 end
