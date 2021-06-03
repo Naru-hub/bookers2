adeceefde95e8afd75492450bf900cbde013ace8
Naru-hub <1208ny0324@gmail.com>
2021-06-03 10:11:57 +0000

    add search form

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,4 +32,19 @@ class User < ApplicationRecord
 
   validates :name, uniqueness: true, length: {in: 2..20 }
   validates :introduction, length: {maximum: 50 }
+
+  #検索方法分岐
+  def self.looks(search,word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?", "#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?", "%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?", "%#{word}%")
+    else
+      @user = User.all
+    end
+  end
 end
