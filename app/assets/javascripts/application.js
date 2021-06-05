4b2432b3a0d613194e0bfe15360b9a7a64e86abf
Naru-hub <1208ny0324@gmail.com>
2021-06-05 07:09:10 +0000

    favorite comment Ajax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,8 @@
 //= require popper
 //= require bootstrap-sprockets
 
+
+
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   get "/search" => "searchs#search"
 
-  resources :users,only:[:index,:show,:edit,:update] do
+  resources :users, only:[:index,:show,:edit,:update] do
      resource :relationships, only: [:create, :destroy]
   get 'followings' => 'relationships#followings', as: 'followings'
   get 'followers' => 'relationships#followers', as: 'followers'
   end
 
-  resources :books,only:[:index,:create,:show,:edit,:update,:destroy] do
+  resources :books, only:[:index, :create, :show, :edit, :update, :destroy] do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
