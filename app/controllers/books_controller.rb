1d3fbb116a69f655163463504aef023ef90dc567
Naru-hub <1208ny0324@gmail.com>
2021-06-08 05:55:41 +0000

    add times research date

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,8 +2,13 @@ class BooksController < ApplicationController
   before_action :baria_user, only: [:edit]
 
   def index
-    @books = Book.all.includes(:favorited_users).sort {|a,b|b.favorited_users.size <=>
-    a.favorited_users.size}
+    to  = Time.current.at_end_of_day
+    from  = (to - 6.day).at_beginning_of_day
+    @books = Book.all.includes(:favorited_users).
+    sort {|a,b|
+    b.favorited_users.where(created_at: from...to).size <=>
+    a.favorited_users.where(created_at: from...to).size
+    }
     @user = current_user
     @book = Book.new
   end
--- app/models/book.rb
+++ app/models/book.rb
@@ -27,4 +27,16 @@ class Book < ApplicationRecord
     end
   end
 
+  #今日投稿されたBookを取得
+  scope :created_today, -> { where("created_at >= ?", Time.zone.now.beginning_of_day)}
+  #前日投稿されたBookを取得
+  scope :created_yesterday, -> { where(created_at: Time.zone.now.all_day)}
+
+
+  #今週投稿されたBookを取得
+ scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
+
+  #全集投稿されたBookを取得
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day)}
+
 end
